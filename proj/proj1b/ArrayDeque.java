af044e14ab0bca75bd872ee3cc387c6a9cdb640b
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-05-24 01:17:53 -0500

    Changed to my own deque implemetation

---
+++ proj/proj1b/ArrayDeque.java
@@ -0,0 +1,382 @@
+/**  Project 1A: Circular ArrayDeque.
+ *
+ *   ref = "https://sp18.datastructur.es/materials/proj/proj1a/proj1a"
+ *
+ *   @author Zhenye Na 05/19/2018
+ *
+ * */
+
+import java.util.Random;
+
+public class ArrayDeque<Item> implements Deque<Item> {
+
+    /** Declare variables. */
+    private float usage;             // Usage factor
+    private int size;                // size of Array-Deque
+    private int nextFirst;           // First pointer
+    private int nextLast;            // Last pointer
+    private Item[] Array; // Data Structure used in Array-Deque
+
+
+    /** Create an empty Array Deque. */
+    public ArrayDeque() {
+        Array = (Item[]) new Object[8];
+        size = 0;
+        usage = 0;
+
+        // whether initialize nextFirst randomly or manually.
+        // nextFirst = getRandomNumberInRange(0, 7);
+        nextFirst = 2;
+
+        // make sure that initial nextLast is on the RHS of nextFirst
+        if (nextFirst == 7) {
+            nextLast = 0;
+        } else {
+            nextLast = nextFirst + 1;
+        }
+    }
+
+
+    /**  private helper function.
+     *
+     *   Get random integer in given range.
+     *
+     *   @source "https://www.mkyong.com/java/java-generate-random-integers-in-a-range/"
+     *
+     *   Args:
+     *       min (int): lower bound of the range.
+     *       max (int): upper bound of the range.
+     *
+     *   Returns:
+     *       random number (int): random integer in range(min, max + 1).
+     *
+     * */
+    private static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+
+    /**  private helper function
+     *
+     *   Check usage of array, return true will halve the array.
+     * */
+    private boolean checkUsageR() {
+
+        // compare ratio with 0.25
+        return (usage < 0.25 && (Array.length > 16));
+    }
+
+
+    /**  resize an array.
+     *
+     *   1. For arrays of length 16 or more, usage factor should always be at least 25%
+     *      Otherwise halve the array.
+     *      For smaller arrays, usage factor can be arbitrarily low.
+     *
+     *      Usage factor < 25% < 50%. So we can shrink the array size by 2 with no overflow.
+     *
+     *      References:
+     *          Gitbook - Chapter 2.5: Memory Performance
+     *
+     *
+     *   2. Array is full and enlarge the Array.
+     *
+     *      Enlarge Factor = 2
+     *
+     *
+     *   3. My strategy for adjusting capacity of Array:
+     *
+     *      1) shrinking size
+     *
+     *      2) increasing size
+     *
+     *      [current array]: (1 2 3 4 5 6 7 8)
+     *      whenever we want to add a new element, we need increase the capacity of
+     *      current array, the strategy I applied is as follows
+     *
+     *      (1 2 3 4 5 6 7 8) -> (null 1 2 3 4 5 6 7 8 null ...)
+     *
+     *      that means I use `System.arraycopy` function copy all the elements in oringinal
+     *      array to a new array with default starting index `1`. And I move the First pointer
+     *      to the first position of the new Array (nextFirst = 0;) and Last pointer to the last
+     *      element position of the new Array.
+     *
+     *      Though, it seems a little dummy... :)
+     *
+     * */
+    private void resize() {
+
+        // condition 1 and 2
+        if (checkUsageR() || (size == Array.length)) {
+
+            int targetSize = Array.length;
+
+            if (checkUsageR()) {
+                targetSize /= 2;
+            } else {
+                targetSize *= 2;
+            }
+
+            // New Array
+            Item[] NewArray = (Item[]) new Object[targetSize];
+
+            // Copy from the first to the last element of the original array.
+            // Start index setting
+            int maxIndex = 0;
+            int startIndex = 0;
+
+            if (checkUsageR()) {
+                // maxIndex = Math.max(nextFirst, nextLast) + 1;
+
+                // Deal with startIndex when usage is pretty low -> Dummy approach
+                for (int i = 0; i < Array.length - 1; i++) {
+                    if (Array[i] == null && Array[i + 1] != null) {
+                        startIndex = i + 1;
+                    }
+                    if (Array[i] != null && Array[i + 1] == null) {
+                        maxIndex = i;
+                    }
+                }
+
+                System.arraycopy(Array, startIndex, NewArray, 1, maxIndex - startIndex + 1);
+
+                Array = NewArray;
+
+                // pointers setting
+                nextFirst = 0;
+                nextLast = maxIndex - startIndex + 2;
+
+            } else {
+                maxIndex = Array.length;
+                startIndex = 0;
+
+                System.arraycopy(Array, startIndex, NewArray, 1, maxIndex - startIndex);
+
+                Array = NewArray;
+
+                // pointers setting
+                nextFirst = 0;
+                nextLast = maxIndex - startIndex + 1;
+            }
+
+        }
+
+    }
+
+
+    /**  private helper function.
+     *
+     *   move First pointer when adding element.
+     * */
+    private int addpointerFirst(int nextFirst) {
+        if (nextFirst != 0) {
+            // move First pointer backward
+            nextFirst -= 1;
+        } else {
+            // move First pointer to the end of Array.
+            nextFirst = Array.length - 1;
+        }
+        return nextFirst;
+    }
+
+
+    /**  private helper function.
+     *
+     *   move Last pointer when adding element.
+     * */
+    private int addpointerLast(int nextLast) {
+        if (nextLast != Array.length - 1) {
+            // move Last pointer forward
+            nextLast += 1;
+        } else {
+            // move First pointer to the end of Array.
+            nextLast = 0;
+        }
+        return nextLast;
+    }
+
+
+    /**  Adds an item of type <Item> to the front of the deque.
+     *
+     *   Args:
+     *       item (Item): new first item.
+     *
+     *   Returns:
+     *       Nothing to return.
+     * */
+    @Override
+    public void addFirst(Item item) {
+
+        // Check usage factor.
+        resize();
+
+        Array[nextFirst] = item;
+        size += 1;
+        usage = (float) size / (float) Array.length;
+
+        // move the pointer of nextFirst.
+        nextFirst = addpointerFirst(nextFirst);
+    }
+
+
+    /**  Adds an item of type <Item> to the back of the deque.
+     *
+     *   Args:
+     *       item (Item): new first item.
+     *
+     *   Returns:
+     *       Nothing to return.
+     * */
+    @Override
+    public void addLast(Item item) {
+
+        // Check usage factor.
+        resize();
+
+        Array[nextLast] = item;
+        size += 1;
+        usage = (float) size / (float) Array.length;
+
+        // move the pointer of nextLast.
+        nextLast = addpointerLast(nextLast);
+    }
+
+
+    /**  Returns true if deque is empty, false otherwise.*/
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    /**  Returns the number of items in the deque in constant time. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    /**  Prints the items in the deque from first to last, separated by a space. */
+    @Override
+    public void printDeque() {
+        for (Item p : Array) {
+            System.out.print(p + " ");
+        }
+        System.out.print("\n");
+    }
+
+
+    /**  private helper function.
+     *
+     *   move First pointer when removing element.
+     * */
+    private int rmpointerFirst(int nextFirst) {
+        if (nextFirst != Array.length - 1) {
+            nextFirst += 1;
+        } else {
+            nextFirst = 0;
+        }
+        return nextFirst;
+    }
+
+
+    /**  private helper function.
+     *
+     *   move Last pointer when removing element.
+     * */
+    private int rmpointerLast(int nextLast) {
+        if (nextLast != 0) {
+            // move Last pointer forward
+            nextLast -= 1;
+        } else {
+            // move First pointer to the end of Array.
+            nextLast = Array.length - 1;
+        }
+        return nextLast;
+    }
+
+
+    /**  Removes and returns the item at the front of the deque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       None
+     *
+     *   Returns:
+     *       First item in Circular ArrayDeque or null.
+     * */
+    @Override
+    public Item removeFirst() {
+
+        int index = rmpointerFirst(nextFirst);
+        Item returnValue = Array[index];
+        Array[index] = null;
+        size -= 1;
+        usage = (float) size / (float) Array.length;
+
+        // move pointer
+        nextFirst = index;
+
+        // Check usage factor.
+        resize();
+
+        return returnValue;
+    }
+
+
+    /**  Removes and returns the item at the back of the deque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       None
+     *
+     *   Returns:
+     *       Last item in Circular ArrayDeque or null
+     * */
+    @Override
+    public Item removeLast() {
+
+        int index = rmpointerLast(nextLast);
+        Item returnValue = Array[index];
+        Array[index] = null;
+        size -= 1;
+        usage = (float) size / (float) Array.length;
+
+        // move pointer
+        nextLast = index;
+
+        // Check usage factor.
+        resize();
+
+        return returnValue;
+
+    }
+
+
+    /**  Get i^{th} item in the Circular ArrayDeque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       index (int): index of the target item.
+     *
+     *   Returns:
+     *       index^{th} item in the deque.
+     *
+     * */
+    public Item get(int index) {
+        if (index >= this.Array.length) {
+            return null;
+        } else {
+            return this.Array[index];
+        }
+
+    }
+
+}
+
--- proj/proj1b/LinkedListDeque.java
+++ proj/proj1b/LinkedListDeque.java
@@ -1,31 +1,231 @@
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
+/**  Project 1A: Linked List Deque.
+ *
+ *   ref = "https://sp18.datastructur.es/materials/proj/proj1a/proj1a"
+ *
+ *   @author Zhenye Na 05/18/2018
+ *
+ * */
 
-/**
- * Isn't this solution kinda... cheating? Yes.
- */
-public class LinkedListDeque<Item> extends LinkedList<Item> implements Deque<Item> {
+public class LinkedListDeque<Item> implements Deque<Item> {
+
+    /**  private helper class for node in Linked-List-Deque.
+     *
+     *   Circular Sentinel
+     *
+     * */
+    private class ListNode {
+        private Item item;
+        private ListNode prev;
+        private ListNode next;
+
+        private ListNode(ListNode p, Item i, ListNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+
+    }
+
+
+    /** Declare variables. */
+    private ListNode sentinel;
+    private int size;
+
+
+    /**  Creates an empty linked list deque. */
+    public LinkedListDeque() {
+        sentinel = new ListNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+
+    /**  Adds an item of type <Item> to the front of the deque.
+     *
+     *   Args:
+     *       item (Item): new first item.
+     *
+     *   Returns:
+     *       Nothing to return.
+     * */
+    @Override
+    public void addFirst(Item item) {
+        sentinel.next = new ListNode(sentinel, item, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+        size += 1;
+    }
+
+
+    /**  Adds an item of type <Item> to the back of the deque.
+     *
+     *   Args:
+     *       item (Item): new first item.
+     *
+     *   Returns:
+     *       Nothing to return.
+     * */
+    @Override
+    public void addLast(Item item) {
+        sentinel.prev.next = new ListNode(sentinel.prev, item, sentinel);
+        sentinel.prev = sentinel.prev.next;
+        size += 1;
+    }
+
+
+    /**  Returns true if deque is empty, false otherwise.*/
+    @Override
+    public boolean isEmpty() {
+        return (this.size == 0);
+    }
+
+
+    /**  Returns the number of items in the deque in constant time. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    /**  Prints the items in the deque from first to last, separated by a space. */
+    @Override
     public void printDeque() {
-        System.out.println("dummy");
+        ListNode node = sentinel;
+        while (node.next != sentinel) {
+            System.out.print(node.next.item + " ");
+            node = node.next;
+        }
     }
 
-    public Item getRecursive(int i) {
-        return get(i);
-    }
 
+    /**  Removes and returns the item at the front of the deque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       None
+     *
+     *   Returns:
+     *       First item in Linked-List-Deque or null
+     * */
+    @Override
     public Item removeFirst() {
-        try {
-            return super.removeFirst();
-        } catch (NoSuchElementException e) {
+
+        if (size == 0) {
+            // no such item exists, return null
             return null;
+        } else {
+            // remove first item and return that item
+            Item firstItem = sentinel.next.item;
+            sentinel.next.next.prev = sentinel;
+            sentinel.next = sentinel.next.next;
+            size -= 1;
+
+            return firstItem;
+        }
+
+    }
+
+
+    /**  Removes and returns the item at the back of the deque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       None
+     *
+     *   Returns:
+     *       Last item in Linked-List-Deque or null
+     * */
+    @Override
+    public Item removeLast() {
+
+        if (size == 0) {
+            // no such item exists, return null
+            return null;
+        } else {
+            // remove last item and return that item
+            Item lastItem = sentinel.prev.item;
+            sentinel.prev.prev.next = sentinel;
+            sentinel.prev = sentinel.prev.prev;
+            size -= 1;
+
+            return lastItem;
+        }
+
+    }
+
+
+    /**  Gets the item at the given index using <iteration>
+     *   where 0 is the front, 1 is the next item, and so forth.
+     *   If no such item exists, returns null.
+     *
+     *   <Must not alter the deque!>
+     *
+     *   Args:
+     *       i (int): index.
+     *
+     *   Returns:
+     *       index^{th} item in the deque.
+     * */
+    public Item getIterative(int index) {
+        int length = size;
+        ListNode target = sentinel.next;
+
+        // no such item exists, returns null.
+        if (index > length - 1) {
+            return null;
+        } else {
+            for (int i = 0; i < index; i++) {
+                target = target.next;
+            }
+
+            return target.item;
+        }
+
+    }
+
+
+    /**  Gets the item at the given index using <recursion>
+     *   where 0 is the front, 1 is the next item, and so forth.
+     *   If no such item exists, returns null.
+     *
+     *   <Must not alter the deque!>
+     *
+     *   Args:
+     *       i (int): index.
+     *
+     *   Returns:
+     *       index^{th} item in the deque.
+     * */
+    public Item getRecursive(int index) {
+        int length = size;
+
+        // no such item exists, returns null.
+        if (index > length - 1) {
+            return null;
+        } else {
+            return traverse(sentinel.next, index);
+        }
+
+    }
+
+
+    /**  getRecursive helper function.
+     *
+     *   Args:
+     *       n (ListNode): current pointer.
+     *       i (int): index.
+     *
+     *   Returns:
+     *       target item (Item).
+     * */
+    public Item traverse(ListNode n, int i) {
+
+        if (i == 0) {
+            return n.item;
+        } else {
+            return traverse(n.next, i - 1);
         }
     }
 
-    public Item removeLast() {
-        try {
-            return super.removeLast();
-        } catch (NoSuchElementException e) {
-            return null;
-        }
-    }
+
 }
