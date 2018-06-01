a839370613fecd6efe903bd9359e4e5344998ad5
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-05-31 19:23:23 -0500

    Add MyHashMap.java

---
+++ lab/lab9/lab9/MyHashMap.java
@@ -0,0 +1,116 @@
+package lab9;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ *  A hash table-backed Map implementation. Provides amortized constant time
+ *  access to elements via get(), remove(), and put() in the best case.
+ *
+ *  @author Zhenye Na, created on 05/31/2018
+ */
+public class MyHashMap<K, V> implements Map61B<K, V> {
+
+    private static final int DEFAULT_SIZE = 16;
+    private static final double MAX_LF = 0.75;
+
+    private ArrayMap<K, V>[] buckets;
+    private int size;
+
+    private int loadFactor() {
+        return size / buckets.length;
+    }
+
+    public MyHashMap() {
+        buckets = new ArrayMap[DEFAULT_SIZE];
+        this.clear();
+    }
+
+    /* Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        this.size = 0;
+        for (int i = 0; i < this.buckets.length; i += 1) {
+            this.buckets[i] = new ArrayMap<>();
+        }
+    }
+
+    /** Computes the hash function of the given key. Consists of
+     *  computing the hashcode, followed by modding by the number of buckets.
+     *  To handle negative numbers properly, uses floorMod instead of %.
+     */
+    private int hash(K key) {
+        if (key == null) {
+            return 0;
+        }
+
+        int numBuckets = buckets.length;
+        return Math.floorMod(key.hashCode(), numBuckets);
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        if (this.buckets[hash(key)].containsKey(key)) {
+            return this.buckets[hash(key)].get(key);
+        } else {
+            return null;
+        }
+        // throw new UnsupportedOperationException();
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+
+        if (this.buckets[hash(key)].containsKey(key)) {
+            this.buckets[hash(key)].put(key, value);
+        } else {
+            this.buckets[hash(key)].put(key, value);
+            size += 1;
+        }
+
+        // size += this.buckets[hash(key)].size;
+        // throw new UnsupportedOperationException();
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size;
+        // throw new UnsupportedOperationException();
+    }
+
+
+
+    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the mapping for the specified key from this map if exists.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for this lab. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
