99be4455c0e77f28403e99ebd3c191c829d1be23
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-06-09 17:56:05 -0500

    Finished part1

--- hw/hw2/hw2/Percolation.java
+++ hw/hw2/hw2/Percolation.java
@@ -2,10 +2,17 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Percolation {
 
     private int[][] grid;
+    private boolean[][] status;
+
     private int numOfOpen;
+    private int head;
+    private int tail;
 
     private WeightedQuickUnionUF WQU;
 
@@ -17,9 +24,13 @@ public class Percolation {
             throw new IllegalArgumentException(N + " is not smaller or equal to 0! ");
         }
 
-        grid = new int[N][N]; //default value is 0.
-        numOfOpen = 0;
-        WQU = new WeightedQuickUnionUF(matrix2vector(N - 1, N - 1));
+        grid = new int[N][N];                              // the chess board of our continent, by default each element in grid is 0.
+        status = new boolean[N][N];                        // status (open or not) of each site, by default each element in status is false
+        numOfOpen = 0;                                     // number of open site in the grid
+        WQU = new WeightedQuickUnionUF(N * N + 2);      // WeightedQuickUnionUF data structures
+
+        head = N * N;                                      // virtual top site
+        tail = N * N + 1;                                  // virtual bottom site
 
     }
 
@@ -44,7 +55,7 @@ public class Percolation {
      */
     private int matrix2vector(int p, int q) {
         int n = grid.length;
-        return p * n + q + 1;
+        return p * n + q;
     }
 
 
@@ -56,11 +67,13 @@ public class Percolation {
 
         // Check whether it is open
         if (isOpen(row, col)) {
+            throw new IllegalArgumentException("This site is already opened!");
+        } else {
             grid[row][col] = 1;
             numOfOpen += 1;
+            Connect(row, col);
+            status[row][col] = true;
         }
-        Connect(row, col);
-
     }
 
 
@@ -70,31 +83,52 @@ public class Percolation {
      * connect the two components
      */
     private void Connect(int p, int q) {
-        int cord = matrix2vector(p, q);
-        int[] neighbors = new int[] {cord - 1, cord + 1, cord - grid.length, cord + grid.length};
+        int curr = matrix2vector(p, q);
+        List<Integer> neighbors = new ArrayList<>();
 
+        // first row in the grid, connect to the head
+        if (p == 0) WQU.union(head, curr);
+
+        // bottom row in the grid, connect to the tail
+        if (p == grid.length - 1) WQU.union(tail, curr);
+
+        // Check current site's neighbours is opened or not
+        if (p != 0 && isOpen(p - 1, q)) neighbors.add(matrix2vector(p - 1, q));
+        if (p != (grid.length - 1) && isOpen(p + 1, q)) neighbors.add(matrix2vector(p + 1, q));
+        if (q != 0 && isOpen(p, q - 1)) neighbors.add(matrix2vector(p, q - 1));
+        if (q != (grid.length - 1) && isOpen(p, q + 1)) neighbors.add(matrix2vector(p, q + 1));
+
+        // Union the opened sites
+        for (int i : neighbors) {
+            WQU.union(curr, i);
+        }
 
 
     }
 
 
-    /** is the site (row, col) open? */
+    /** is the site (row, col) open?
+     *  whether click this site?
+     */
     public boolean isOpen(int row, int col) {
         // validate whether index is legal
         validate(row);
         validate(col);
 
-        return grid[row][col] == 1;
+        return status[row][col];
     }
 
 
-    /** is the site (row, col) full? */
+    /** is the site (row, col) full?
+     *  whether connected to the top?
+     */
     public boolean isFull(int row, int col) {
         // validate whether index is legal
         validate(row);
         validate(col);
 
-        return grid[row][col] == 1 && ;
+        // Check this site is connected to head or not
+        return WQU.connected(head, matrix2vector(row, col));
     }
 
 
@@ -111,14 +145,17 @@ public class Percolation {
     /** does the system percolate? */
     public boolean percolates() {
 
-        // Dummy version for checking percolation
-        for (int i = 0; i < grid.length; i += 1) {
-            if (WQU.connected(0, i)) {
-                return true;
-            }
-        }
+//        // Dummy version for checking percolation
+//        for (int i = 0; i < grid.length; i += 1) {
+//            if (WQU.connected(0, i)) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
 
-        return false;
+        // Check tail is connected to head or not
+        return WQU.connected(head, tail);
 
     }
 
